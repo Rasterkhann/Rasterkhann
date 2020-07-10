c7944ea18921c0bf563d335d34d2c424351902ae
seiyria <kyle@seiyria.com>
2020-07-10 16:02:47 -0500

    feat(inn): info panel

--- src/app/components/windows/inn/inn.component.ts
+++ src/app/components/windows/inn/inn.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { IGameTown } from '../../../interfaces';
+import { calculateRestingRate, calculateRestingCost } from '../../../helpers';
 
 @Component({
   selector: 'app-inn',
@@ -9,6 +10,14 @@ import { IGameTown } from '../../../interfaces';
 export class InnComponent implements OnInit {
 
   @Input() town: IGameTown;
+
+  public get restingRate(): number {
+    return calculateRestingRate(this.town);
+  }
+
+  public get goldPerRestTick(): number {
+    return calculateRestingCost(this.town);
+  }
 
   constructor() { }
 
