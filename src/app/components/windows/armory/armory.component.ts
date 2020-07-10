15e94ebe357e176f688f3b01a5b8756ee1c26019
seiyria <kyle@seiyria.com>
2020-07-10 15:41:38 -0500

    feat(armory): quick info for armory highlights

--- src/app/components/windows/armory/armory.component.ts
+++ src/app/components/windows/armory/armory.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { IGameTown } from '../../../interfaces';
+import { IGameTown, ItemType } from '../../../interfaces';
+import { calculateMaxCreatableItems, calculateSecondsUntilNextItem } from '../../../helpers';
 
 @Component({
   selector: 'app-armory',
@@ -9,6 +10,22 @@ import { IGameTown } from '../../../interfaces';
 export class ArmoryComponent implements OnInit {
 
   @Input() town: IGameTown;
+
+  public get maximumArmors(): number {
+    return calculateMaxCreatableItems(this.town, ItemType.Armor);
+  }
+
+  public get armorCreationTime(): number {
+    return calculateSecondsUntilNextItem(this.town, ItemType.Armor);
+  }
+
+  public get maximumWeapons(): number {
+    return calculateMaxCreatableItems(this.town, ItemType.Weapon);
+  }
+
+  public get weaponCreationTime(): number {
+    return calculateSecondsUntilNextItem(this.town, ItemType.Weapon);
+  }
 
   constructor() { }
 
