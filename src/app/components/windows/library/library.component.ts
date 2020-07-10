6a44998232b29b3493a50c68da759e2ffe4d347d
seiyria <kyle@seiyria.com>
2020-07-10 16:03:58 -0500

    feat(library): info panel

--- src/app/components/windows/library/library.component.ts
+++ src/app/components/windows/library/library.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { IGameTown } from '../../../interfaces';
+import { calculateMaxNumberOfTraits } from '../../../helpers';
 
 @Component({
   selector: 'app-library',
@@ -9,6 +10,10 @@ import { IGameTown } from '../../../interfaces';
 export class LibraryComponent implements OnInit {
 
   @Input() town: IGameTown;
+
+  public get maxTraits(): number {
+    return calculateMaxNumberOfTraits(this.town);
+  }
 
   constructor() { }
 
