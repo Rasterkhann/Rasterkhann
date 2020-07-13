c1e0b74999b3c42134eb65a1f5ce09a877f005c5
Kyle J. Kemp <kyle@seiyria.com>
2020-07-13 08:23:04 -0500

    feat(bazaar): show totals at top of headers

--- src/app/components/windows/bazaar/items-modal/items-modal.component.ts
+++ src/app/components/windows/bazaar/items-modal/items-modal.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 
 import { GameService } from '../../../../services/game.service';
-import { IGameTown } from '../../../../interfaces';
+import { HeroItem, IGameTown, ItemType } from '../../../../interfaces';
+import { calculateMaxCreatableItems } from '../../../../helpers';
 
 @Component({
   selector: 'app-items-modal',
@@ -19,6 +20,14 @@ export class ItemsModalComponent implements OnInit {
 
   dismiss(): void {
     this.modal.dismiss();
+  }
+
+  public getTypeTotal(type: ItemType): number {
+    return calculateMaxCreatableItems(this.town, type);
+  }
+
+  public getItems(type: ItemType): HeroItem[] {
+    return this.town.itemsForSale[type];
   }
 
 }
