2e7e2024cf606b9676aa560793224bf33f6e4aac
Kyle J. Kemp <kyle@seiyria.com>
2020-07-13 13:52:21 -0500

    feat(workshop): workshop costs lowered to 5k each

--- src/app/features/workshop.ts
+++ src/app/features/workshop.ts
@@ -4,21 +4,21 @@ export const WorkshopFeatures: BuildingFeature[] = [
   {
     name: 'Automation: Upgrade Buildings',
     description: 'Unlock an option to automatically upgrade buildings.',
-    cost: 1_000_000n,
+    cost: 5000n,
     upgradeTime: 3600,
     requiresLevel: 1
   },
   {
     name: 'Automation: Train Heroes',
     description: 'Unlock an option to automatically train heroes.',
-    cost: 1_000_000n,
+    cost: 5000n,
     upgradeTime: 3600,
     requiresLevel: 1
   },
   {
     name: 'Automation: Adventure',
     description: 'Unlock an option to automatically go on adventures.',
-    cost: 1_000_000n,
+    cost: 5000n,
     upgradeTime: 3600,
     requiresLevel: 1
   },
