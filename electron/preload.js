5401c62bae5e6785ecaab6ff0895d5df0a63b092
Kyle J. Kemp <kyle@seiyria.com>
2020-07-15 18:43:52 -0500

    feat(core): in downloadable version, back up savefile if it exists but failed to load
    closes #42

--- electron/preload.js
+++ electron/preload.js
@@ -5,21 +5,27 @@ const os = require('os');
 const { compressToUTF16, decompressFromUTF16 } = require('lz-string');
 
 const folder = os.homedir() + '/Documents/Rasterkhann';
+const savefileLoc = path.join(folder, 'Rasterkhann.sav');
 
 window.isDownloaded = true;
 
 window.saveCurrentSavefile = (saveData) => {
   fs.mkdirSync(folder, { recursive: true });
-  fs.writeFileSync(path.join(folder, 'Rasterkhann.sav'), compressToUTF16(JSON.stringify(saveData)), 'UTF-8');
+  fs.writeFileSync(savefileLoc, compressToUTF16(JSON.stringify(saveData)), 'UTF-8');
 };
 
 window.loadCurrentSavefile = () => {
   try {
 
-    const baseFile = JSON.parse(decompressFromUTF16(fs.readFileSync(path.join(folder, 'Rasterkhann.sav'), 'UTF-8')));
+    const baseFile = JSON.parse(decompressFromUTF16(fs.readFileSync(savefileLoc, 'UTF-8')));
     return baseFile;
   } catch(e) {
 
+    // back up existing savefile in case loading it is busted
+    if(fs.existsSync(savefileLoc)) {
+      fs.copyFileSync(savefileLoc, path.join(folder, `Rasterkhann-${Date.now()}.sav`));
+    }
+    
     return {};
   }
 };
