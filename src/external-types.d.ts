07ed213e71bb8406d25a5ce0f14c3860d2e68438
Kyle J. Kemp <kyle@seiyria.com>
2020-07-20 07:10:56 -0500

    feat(core): shim bigint, so the game should work on unsupported environments like safari
    closes #62

--- src/external-types.d.ts
+++ src/external-types.d.ts
@@ -1,3 +1,4 @@
 
 declare module 'swarm-numberformat';
 declare module 'file-saver';
+declare module 'bigint-polyfill';
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -64,3 +64,6 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+import BigInt from 'bigint-polyfill';
+(window as any).BigInt = window.BigInt || BigInt;
